fffc29a1fdd8d8a233596a63f5482e293d0d6285
khaled al shishani <khaled.shishani32@gmail.com>
2021-07-25 19:22:36 +0300

    finish with Stretch Goals

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,64 @@
+menu=['wings','cookies','spring rolls','salmon','steak','meat tornado','a literal garden','ice cream','cake','pie','coffee','tea','unicorn tears']
+
+
+print("""**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************""")
+
+print("""Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears""")
+
+order=[]
+count =0 
+print("""***********************************
+** What would you like to order? **
+***********************************""")
+
+
+    
+
+while(-1 <0):
+  
+ order_item = input(">")
+ order_item=order_item.lower()
+ if order_item in menu:
+  order.append(order_item)
+  count = order.count(order_item)
+  print(f" ** {count} order of {order_item} have been added to your meal **")
+  
+ 
+ elif order_item == "quit":
+     exit() 
+
+ else:
+  print("not aviable , chooes something else")
+ 
+
+
+
+
